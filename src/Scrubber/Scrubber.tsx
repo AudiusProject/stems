c5a8dd5ccc9901b17a614704cb0463bcaa44de7b
Raymond Jacobson <ray@audius.co>
2019-09-20 12:23:35 -0700

    Reformat

--- src/Scrubber/Scrubber.tsx
+++ src/Scrubber/Scrubber.tsx
@@ -6,7 +6,9 @@ import Slider from './Slider'
 import ScrubberProps, { defaultScrubberProps } from './types'
 import styles from './Scrubber.module.css'
 
-const ONE_HOUR_IN_SECONDS = 60 /* seconds */ * 60 /* minutes */
+const SECONDS_PER_MINUTE = 60
+const MINUTES_PER_HOUR = 60
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR
 
 /** Timeout applied when releasing the drag-handle before timestamps reset. */
 const SCRUB_RELEASE_TIMEOUT_MS = 200
@@ -14,7 +16,7 @@ const SCRUB_RELEASE_TIMEOUT_MS = 200
 /** Pretty formats seconds into m:ss. */
 const formatSeconds = (seconds: number) => {
   const utc = moment.utc(moment.duration(seconds, 'seconds').asMilliseconds())
-  if (seconds > ONE_HOUR_IN_SECONDS) {
+  if (seconds > SECONDS_PER_HOUR) {
     return utc.format('h:mm:ss')
   }
   return utc.format('m:ss')
