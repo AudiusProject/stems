d889c66b5b310b0f8535132e98093a5adb3bfc0c
sliptype <sliptype@gmail.com>
2021-07-02 10:28:17 -0500

    Update paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import 'assets/styles/sizes.css'
-import 'assets/styles/fonts.css'
-import 'assets/styles/colors.css'
-import 'assets/styles/animations.css'
+import './assets/styles/sizes.css'
+import './assets/styles/fonts.css'
+import './assets/styles/colors.css'
+import './assets/styles/animations.css'
 
 export * from 'components/Icons'
 
@@ -10,19 +10,19 @@ export {
   ButtonProps,
   Type as ButtonType,
   Size as ButtonSize
-} from './components/Button'
-export { Scrubber } from './components/Scrubber'
-export { Modal, ModalProps, Anchor } from './components/Modal'
-export { TabSlider, TabSliderProps, Option } from './components/TabSlider'
+} from 'components/Button'
+export { Scrubber } from 'components/Scrubber'
+export { Modal, ModalProps, Anchor } from 'components/Modal'
+export { TabSlider, TabSliderProps, Option } from 'components/TabSlider'
 export {
   TokenValueSlider,
   TokenValueSliderProps
-} from './components/TokenValueSlider'
+} from 'components/TokenValueSlider'
 export {
   TokenValueInput,
   TokenValueInputProps,
   Format
-} from './components/TokenValueInput'
+} from 'components/TokenValueInput'
 
 export { useHotkeys } from 'hooks/useHotKeys'
 export { useClickOutside } from 'hooks/useClickOutside'
